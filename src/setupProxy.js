7c8cbd708a83e823ab8f4bd1bd498cf915038751
coder-akash <akashd@brightcomgroup.com>
2024-10-09 15:12:42 +0530

    ready to deploy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,9 +2,8 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/api',
-    createProxyMiddleware({
-      target: 'https://dm-backend-4sss.onrender.com',
+    createProxyMiddleware('/api',{
+      target: process.env.REACT_APP_BACKEND_SERVER,
       changeOrigin: true,
     })
   );
