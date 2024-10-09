c070cb515793ca82becfadff236c473b35263c87
coder-akash <akashd@brightcomgroup.com>
2024-10-09 12:16:00 +0530

    backend api setup

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: process.env.REACT_APP_BACKEND_SERVER,
+      target: 'https://dm-backend-4sss.onrender.com',
       changeOrigin: true,
     })
   );
