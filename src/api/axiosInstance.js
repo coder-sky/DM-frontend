a1ce4462f3431973f6d8c1ea1d1eca15822cebb5
coder-akash <akashd@brightcomgroup.com>
2024-10-10 16:00:25 +0530

    api configuration

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,6 +2,7 @@ import axios from "axios"
 
 const instance = axios.create({
     withCredentials: true,
+    credentials: 'include',
     baseURL: process.env.REACT_APP_BACKEND_SERVER,
     headers: {
         'Access-Control-Allow-Origin' : '*',
